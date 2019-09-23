1ac502d22989f161b765d9b84e08f6668ca86105
Subho Halder <subho.halder@gmail.com>
2019-09-24 01:54:33 +0530

    Update apkverify from upstream to appknox pypi

--- apkverify/metadata.py
+++ apkverify/metadata.py
@@ -3,10 +3,10 @@ __all__ = (
     "__author_email__", "__license__", 'ApkSignature',
 )
 
-__title__ = 'apkverify'
-__description__ = 'Jar Signature / APK Signature v2 verify with pure python (support rsa dsa ecdsa).'
+__title__ = 'ak-apkverify'
+__description__ = 'Appknox Fork - Jar Signature / APK Signature v2 verify with pure python (support rsa dsa ecdsa).'
 __url__ = 'https://github.com/shuxin/apk-signature-verify'
-__version__ = '0.1.1'
+__version__ = '0.1.1.1'
 __author__ = 'shuxin'
 __author_email__ = 'shuxin@users.noreply.github.com'
 __license__ = 'MIT License'
